3df706127b6b71880d81f73b434b418ba4f6ce42
Gianfranco De Paula <baciliogianfranco@gmail.com>
2022-04-27 14:00:34 -0400

    Ignorando archivos

--- BackEnd/NotesApp/settings.py
+++ BackEnd/NotesApp/settings.py
@@ -12,17 +12,19 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ij6!*p@7c@c9ypd2zl@y@6lwkwgbg&3trmmo^ukc6us9bsd4@+'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
